03b5cb06aa823adbcaf474d8ad4bd135b648f179
Olalekan Akintola <akinlekan@gmail.com>
2020-11-02 18:12:25 +0100

    build: fixed package.json import error

--- scripts/pre-install.js
+++ scripts/pre-install.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 const path = require('path');
 const { execSync } = require('child_process');
-const { name, version } = require('../../package.json');
+const { name, version } = require('../package.json');
 
-const rootDirectory = path.join(__dirname, '../..');
+const rootDirectory = path.join(__dirname, '../');
 
 // 1. Remove all *.tgz before install
 execSync('rm -rf *.tgz', { cwd: rootDirectory });
